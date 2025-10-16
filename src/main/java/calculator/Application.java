1b7150352ce11013b20ceca9be1091105bf7b80d
DeveloperYeti <hoseok9823@gmail.com>
2025-10-16 15:29:32 +0900

    sout IllArgumentException 삭제.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,5 +5,9 @@ import method.Method;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+
+
+
     }
 }
--- src/main/java/method/Method.java
+++ src/main/java/method/Method.java
@@ -2,9 +2,6 @@ package method;
 import camp.nextstep.edu.missionutils.test.NsTest;
 
 public class Method {
-    // IllegalArgurmentException 메세지 출력
-    public void IllegalArgurmentException(){
-        System.out.println("IllegalArgurmentException");
-    }
+
 
 }
