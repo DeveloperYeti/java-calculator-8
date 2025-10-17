f0c2cd7be9df6d98deb23c5970b2dfcd3da0f41a
DeveloperYeti <hoseok9823@gmail.com>
2025-10-17 21:05:14 +0900

    try Catch문을 통해서 IllgalArgumentException e 예외 처리 앞에서 적은 메서드 Exit 삭제

--- src/main/java/method/Method.java
+++ src/main/java/method/Method.java
@@ -2,6 +2,13 @@ package method;
 import camp.nextstep.edu.missionutils.test.NsTest;
 
 public class Method {
+    public static int add(String input) {
+        // input이 문자열을 참조하지 않거나 ""이 비어있을 경우.
+        if (input == null || input.isEmpty()){
+            throw new IllegalArgumentException("입력 값이 비어 있습니다.");
+        };
 
+        return 0;
+    }
 
 }
