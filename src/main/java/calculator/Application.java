b05cfc0331b64f2158521bebcd163482b5868fef
DeveloperYeti <hoseok9823@gmail.com>
2025-10-17 21:05:03 +0900

    try Catch문을 통해서 IllgalArgumentException e 예외 처리 앞에서 적은 메서드 Exit 삭제

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,20 @@
 package calculator;
-import camp.nextstep.edu.missionutils.test.NsTest;
+import camp.nextstep.edu.missionutils.Console;
 import method.Method;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-
+        try {
+            String input = Console.readLine();
+            int sum = Method.add(input);
+            System.out.println(sum);
+        } catch (IllegalArgumentException e) {
+            System.out.println("입력 값이 올바르지 않습니다. " + e.getMessage());
+            // 필요한 다른 에러 처리
+        } finally {
+            Console.close();
+        }
 
 
     }
