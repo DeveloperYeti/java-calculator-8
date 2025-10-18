095217af20b53cbf72c2c2dc30cd0dbc9aebfc27
DeveloperYeti <hoseok9823@gmail.com>
2025-10-18 23:10:18 +0900

    예외를 감지하도록 다시 던지는 로직 추가.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,14 +8,13 @@ public class Application {
         try {
             String input = Console.readLine();
             int sum = Method.add(input);
-            System.out.println(sum);
+            System.out.println("결과 : " + sum);
         } catch (IllegalArgumentException e) {
             System.out.println("입력 값이 올바르지 않습니다. " + e.getMessage());
-            // 필요한 다른 에러 처리
+            // 테스트에서 예외를 감지하도록 다시 던짐
+            throw e;
         } finally {
             Console.close();
         }
-
-
     }
 }
